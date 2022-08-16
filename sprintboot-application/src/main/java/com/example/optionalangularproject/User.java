84e75250fcca6614deb8ed7b935bf91a17a16bb7
Moe <m.ibrahim0228@gmail.com>
2022-08-16 17:40:17 -0400

    initial commit

---
+++ sprintboot-application/src/main/java/com/example/optionalangularproject/User.java
@@ -0,0 +1,21 @@
+package com.example.optionalangularproject;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity(name = "Users")
+@Table(name = "\"Users\"")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String email;
+
+    // standard constructors / setters / getters / toString
+}
---
+++ sprintboot-application/src/test/java/com/example/optionalangularproject/OptionalAngularProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.optionalangularproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptionalAngularProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
