6b100d9f10b0e71bb3522bec85e7d39c3db18efc
Moe <m.ibrahim0228@gmail.com>
2022-08-16 19:48:26 -0400

    application works

--- angular-application/angular-app/src/app/app.module.ts
+++ angular-application/angular-app/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { UserService } from './service/user.service';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserFormComponent } from './user-form/user-form.component';
-import { UserService } from './service/user.service';
+
 
 @NgModule({
   declarations: [
