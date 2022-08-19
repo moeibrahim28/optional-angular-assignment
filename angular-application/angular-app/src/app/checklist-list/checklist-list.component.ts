604eee142188d7b317757d67636bcd13d5fdc6c2
Moe <m.ibrahim0228@gmail.com>
2022-08-19 16:50:53 -0400

    download implemented and files formatted

--- angular-application/angular-app/src/app/checklist-list/checklist-list.component.ts
+++ angular-application/angular-app/src/app/checklist-list/checklist-list.component.ts
@@ -23,6 +23,45 @@ export class ChecklistListComponent implements OnInit {
     }
   }
 
+  download_txt(checkList: Checklist) {
+    var csv = '';
+    let counter: number = 1;
+    csv += "User Name: ";
+    csv += checkList.user.name;
+    csv += "\n";
+    csv += "Checklist ID: ";
+    csv += checkList.id;
+    csv += "\n";
+    csv += "Checklist Name: ";
+    csv += checkList.name;
+    csv += "\n";
+
+    csv += "Tags: ";
+    if (checkList.tags.length > 0) {
+      if (checkList.tags.length > 1) {
+        checkList.tags.forEach(tag => csv += tag + ",")
+
+      }
+      else if (checkList.tags.length = 1) {
+        csv += checkList.tags[0]
+      }
+    }
+    csv = csv.slice(0, -1)
+    csv += "\n";
+    checkList.itemList.forEach(item => {
+      csv += " -- \n|  | " + item.name + "\n -- ";
+      counter++;
+      csv += "\n";
+    })
+
+    console.log(csv);
+    var hiddenElement = document.createElement('a');
+    hiddenElement.href = 'data:text;charset=utf-8,' + encodeURI(csv);
+    hiddenElement.target = '_blank';
+    hiddenElement.download = checkList.name + '.txt';
+    hiddenElement.click();
+  }
+
   findChecklistWithTag(allChecklists: Checklist[]): Checklist[] {
     const checklistsWithTag: Checklist[] = new Array;
     for (let i = 0; i < allChecklists.length; i++) {
